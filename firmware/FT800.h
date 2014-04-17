29c9587b733cbd970452cae5e1b41008189214f7
Andrew Rossignol <andrew.rossignol@gmail.com>
2014-04-16 22:08:16 -0400

    FT800 Register Definitions

--- firmware/FT800.h
+++ firmware/FT800.h
@@ -10,11 +10,90 @@
 #include "SystemGpio.h"
 #include "SystemSpi.h"
 
+/* FT800 Instance *************************************************************/
+
 typedef struct FT800_t {
     volatile SystemSpiModule_t *Spi;
     volatile SystemGpioModule_t *CsPort;
     uint32_t CsPin;
 } FT800_t;
+
+/* FT800 Registers ************************************************************/
+
+typedef enum FT800Register_t {
+    FT800Register_ID = 0x102400,
+    FT800Register_FRAMES = 0x102404,
+    FT800Register_CLOCK = 0x102408,
+    FT800Register_FREQUENCY = 0x10240C,
+    FT800Register_CPURESET = 0x10241C,
+    FT800Register_GPIO_DIR = 0x10248C,
+    FT800Register_GPIO = 0x102490,
+    FT800Register_INT_FLAGS = 0x102498,
+    FT800Register_INT_EN = 0x10249C,
+    FT800Register_INT_MASK = 0x1024A0,
+    FT800Register_PWM_HZ = 0x1024C0,
+    FT800Register_PWM_DUTY = 0x1024C4,
+    FT800Register_TRACKER = 0x109000,
+    FT800Register_CMD_READ = 0x1024E4,
+    FT800Register_CMD_WRITE = 0x1024E8,
+    FT800Register_CMD_DL = 0x1024EC,
+    FT800Register_PLAYBACK_START = 0x1024A4,
+    FT800Register_PLAYBACK_LENGTH = 0x1024A8,
+    FT800Register_PLAYBACK_READPTR = 0x1024AC,
+    FT800Register_PLAYBACK_FREQ = 0x1024B0,
+    FT800Register_PLAYBACK_FORMAT = 0x1024B4,
+    FT800Register_PLAYBACK_LOOP = 0x1024B8,
+    FT800Register_PLAYBACK_PLAY = 0x1024BC,
+    FT800Register_VOL_PB = 0x10247C,
+    FT800Register_VOL_SOUND = 0x102480,
+    FT800Register_SOUND = 0x102484,
+    FT800Register_REG_PLAY = 0x102488,
+    FT800Register_TOUCH_MODE = 0x1024F0,
+    FT800Register_TOUCH_ADC_MODE = 0x1024F4,
+    FT800Register_TOUCH_CHARGE = 0x1024F8,
+    FT800Register_TOUCH_SETTLE = 0x1024FC,
+    FT800Register_TOUCH_OVERSAMPLE = 0x102500,
+    FT800Register_TOUCH_RZTHRESH = 0x102504,
+    FT800Register_TOUCH_RAW_XY = 0x102508,
+    FT800Register_TOUCH_RZ = 0x10250C,
+    FT800Register_TOUCH_SCREEN_XY = 0x102510,
+    FT800Register_TOUCH_TAG_XY = 0x102514,
+    FT800Register_TOUCH_TAG = 0x102518,
+    FT800Register_TRANSFORM_A = 0x10251C,
+    FT800Register_TRANSFORM_B = 0x102520,
+    FT800Register_TRANSFORM_C = 0x102524,
+    FT800Register_TRANSFORM_D = 0x102528,
+    FT800Register_TRANSFORM_E = 0x10252C,
+    FT800Register_TRANSFORM_F = 0x102530,
+    FT800Register_TOUCH_DIRECT_XY = 0x102574,
+    FT800Register_TOUCH_DIRECT_Z1Z1 = 0x102578,
+    FT800Register_TAG_X = 0x102470,
+    FT800Register_TAG_Y = 0x102474,
+    FT800Register_TAG = 0x102478,
+    FT800Register_DLSWAP = 0x102450,
+    FT800Register_RENDERMODE = 0x102410,
+    FT800Register_SNAPY = 0x102414,
+    FT800Register_SNAPSHOT = 0x102418,
+    FT800Register_TAP_CRC = 0x102420,
+    FT800Register_TAP_MASK = 0x102424,
+    FT800Register_HCYCLE = 0x102428,
+    FT800Register_HOFFSET = 0x10242C,
+    FT800Register_HSIZE = 0x102430,
+    FT800Register_HSYNC0 = 0x102434,
+    FT800Register_HSYNC1 = 0x102438,
+    FT800Register_VCYCLE = 0x10243C,
+    FT800Register_VOFFSET = 0x102440,
+    FT800Register_VSIZE = 0x102444,
+    FT800Register_VSYNC0 = 0x102448,
+    FT800Register_VSYNC1 = 0x10244C,
+    FT800Register_ROTATE = 0x102454,
+    FT800Register_OUTBITS = 0x102458,
+    FT800Register_DITHER = 0x10245C,
+    FT800Register_SWIZZLE = 0x102460,
+    FT800Register_CSPREAD = 0x102464,
+    FT800Register_PCLK_POL = 0x102468,
+    FT800Register_PCLK = 0x10246C
+} FT800Register_t;
 
 /* FT800 Commands *************************************************************/
 
